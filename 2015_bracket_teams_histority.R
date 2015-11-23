3de9160322d5b97ae4e061a430ae5050bc3a8a60
emilyS2015 <wei.shao@duke.edu>
2015-11-22 22:58:24 -0500

    added

--- 2015_bracket_teams_histority.R
+++ 2015_bracket_teams_histority.R
@@ -16,6 +16,19 @@ win_all=prob[which(prob[,2]==1),]
 win_lose=prob[which(prob[,2]==0),]
 win3_lose1=prob[which(prob[,2]==0.5),]
 lose_all=prob[which(prob[,2]==-1),]
+win1_lose3=prob[which(prob[,2]==-1/3),]
 history=prob[which(!is.na(prob[,2])),]
-dim(history)[1]
-4/19
+
+count=0
+for (i in 1:nrow(history)) {
+    if (history[i,2]<0&tourneySlots2015[history[i,1],7]=="b"){
+       count=count+1 
+    } else{
+        if (history[i,2]>0&tourneySlots2015[history[i,1],7]=="a"){
+        count=count+1
+        } else {count=count}
+        
+    }
+}
+
+RightProb=count/nrow(history)
