b6342de2f4113fd4dd533540ca5d844f7aa6ecef
Yaqian Cheng <cici_1115@hotmail.com>
2015-11-24 20:47:02 -0500

    change code

--- code.R
+++ code.R
@@ -15,10 +15,12 @@ detailedR = rbind(detailedR, detailed2015)
 seeds = read.csv("tourney_seeds.csv")
 
 
-predicting = function(year){
+predicting = function(year,year1){ 
+  # year represents the year we predict
+  # year1 represents years of data we use to train the model
   ## Subset Year info:
   # select regular season data
-  regSeasonYearD = detailedR[which(detailedR$season == year),]
+  regSeasonYearD = detailedR[which(detailedR$season == year1),]
   seedsYear = seeds[which(seeds$season == year),]
   if(year!=2015){
     # extract tournament season data
@@ -33,11 +35,11 @@ predicting = function(year){
   } else {
     tourneyYearD = read.csv("tourney_slots_2015.csv")
     tourneyYearD$wteam = 0
-    tourneyYearD$wteam[which(tourneyYearD$Actual.winning.team=='a')] <- tourneyYearD[which(tourneyYearD$Actual.winning.team == 'a'),'team.a']
-    tourneyYearD$wteam[which(tourneyYearD$Actual.winning.team=='b')] <- tourneyYearD[which(tourneyYearD$Actual.winning.team == 'b'),'team.b']
+    tourneyYearD$wteam[which(tourneyYearD$Actual.winning.team=='a')] = tourneyYearD[which(tourneyYearD$Actual.winning.team == 'a'),'team.a']
+    tourneyYearD$wteam[which(tourneyYearD$Actual.winning.team=='b')] = tourneyYearD[which(tourneyYearD$Actual.winning.team == 'b'),'team.b']
     tourneyYearD$lteam = 0
-    tourneyYearD$lteam[which(tourneyYearD$Actual.winning.team=='a')] <- tourneyYearD[which(tourneyYearD$Actual.winning.team == 'a'),'team.b']
-    tourneyYearD$lteam[which(tourneyYearD$Actual.winning.team=='b')] <- tourneyYearD[which(tourneyYearD$Actual.winning.team == 'b'),'team.a']
+    tourneyYearD$lteam[which(tourneyYearD$Actual.winning.team=='a')] = tourneyYearD[which(tourneyYearD$Actual.winning.team == 'a'),'team.b']
+    tourneyYearD$lteam[which(tourneyYearD$Actual.winning.team=='b')] = tourneyYearD[which(tourneyYearD$Actual.winning.team == 'b'),'team.a']
   }
   for(i in 1:dim(tourneyYearD)[1]){
     tourneyYearD[i, "wSeed"] = as.character(seedsYear[which(seedsYear$team == tourneyYearD$wteam[i]),"seed"])
@@ -282,65 +284,63 @@ predicting = function(year){
   return(list(coef = list(lasso.coef,elastic.coef,ridge.coef,bayes.coef),
               pred.rate = list(lasso.rate,elastic.rate,ridge.rate,bayes.rate)))
 }
-result2012 <- predicting(2012)
-result2013 <- predicting(2013)
-result2014 <- predicting(2014)
-result2015 <- predicting(2015)
+result2012 = predicting(2012,c(2011,2012))
+result2013 = predicting(2013,c(2012,2013))
+result2014  =  predicting(2014,c(2013,2014))
+result2015 = predicting(2015,c(2014,2015))
 
 
+tourneySlots2015 = read.csv("tourney_slots_2015.csv")
 
-
-tourneySlots2015=read.csv("tourney_slots_2015.csv")
-
-winProb=c()
+winProb = c()
 for (i in 1:nrow(tourneySlots2015)){
     #extract games of strong seed A as the winning team in 2003-2014
-    team_a=detailedR[which(detailedR[,3]==tourneySlots2015[i,5]),]
+    team_a = detailedR[which(detailedR[,3] == tourneySlots2015[i,5]),]
     #extract games in which strong seed A beat weak seed B given it won 
-    win_b=team_a[which(team_a[,5]==tourneySlots2015[i,6]),]
+    win_b = team_a[which(team_a[,5] == tourneySlots2015[i,6]),]
     #extract games of weak seed B as the winning team in 2003-2014
-    team_b=detailedR[which(detailedR[,3]==tourneySlots2015[i,6]),]
+    team_b = detailedR[which(detailedR[,3] == tourneySlots2015[i,6]),]
     #extract games in which weak seed B beat strong seed A given it won 
-    win_a=team_b[which(team_b[,5]==tourneySlots2015[i,5]),]
+    win_a = team_b[which(team_b[,5] == tourneySlots2015[i,5]),]
     #given total games strong seed A played against weak seed B
     #calculate proporation strong seed A beat weak seed B 
-    winProb[i]=(dim(win_b)[1]-dim(win_a)[1])/(dim(win_b)[1]+dim(win_a)[1])
+    winProb[i] = (dim(win_b)[1]-dim(win_a)[1])/(dim(win_b)[1]+dim(win_a)[1])
 }
 
 #match each proportion calculated above with index of the game
-prob=cbind(1:67,winProb)
+prob = cbind(1:67,winProb)
 #extract games strong seed A always won
-win_all=prob[which(prob[,2]==1),]
+win_all = prob[which(prob[,2] == 1),]
 #extract games strong seed A won as many times as it was beat by weak seed B
-win_lose=prob[which(prob[,2]==0),]
+win_lose = prob[which(prob[,2] == 0),]
 #extract games strong seed A won three times as it was beat by weak seed B
-win3_lose1=prob[which(prob[,2]==0.5),]
+win3_lose1 = prob[which(prob[,2] == 0.5),]
 #extract games strong seed A was always beat by weak seed B
-lose_all=prob[which(prob[,2]==-1),]
+lose_all = prob[which(prob[,2] == -1),]
 #extract games strong seed A was beat by weak seed B twice as it beat weak seed B
-win1_lose2=prob[which(prob[,2]==-1/3),]
+win1_lose2 = prob[which(prob[,2] == -1/3),]
 #extract all the games that had happened with index of the game in the tournament
-history=prob[which(!is.na(prob[,2])),]
+history = prob[which(!is.na(prob[,2])),]
 
 #record how many times historical results is consistent with actual tournament
-count=0
+count = 0
 for (i in 1:nrow(history)) {
-    if (history[i,2]<0&tourneySlots2015[history[i,1],7]=="b"){
-        count=count+1 
+    if (history[i,2] < 0&tourneySlots2015[history[i,1],7] == "b"){
+        count = count+1 
     } else{
-        if (history[i,2]>0&tourneySlots2015[history[i,1],7]=="a"){
-            count=count+1
-        } else {count=count}
+        if (history[i,2] > 0&tourneySlots2015[history[i,1],7] == "a"){
+            count = count+1
+        } else {count = count}
         
     }
 }
 
 #calculate proportion of historical results is consistent actual tournament
-RightProb=count/nrow(history)
+RightProb = count/nrow(history)
 #create an matrix that stores the previous calculated output
-output=matrix(c(nrow(win_all),length(win3_lose1)-1,
+output = matrix(c(nrow(win_all),length(win3_lose1)-1,
                 length(win1_lose2)-1,nrow(win_lose),
-                nrow(lose_all), count, nrow(history),RightProb),nrow =1,ncol=8)
-colnames(output)=c("W", "W1L1", "W1L2", "W1L1","L","Count of corrected results",
+                nrow(lose_all), count, nrow(history),RightProb),nrow = 1,ncol = 8)
+colnames(output) = c("W", "W1L1", "W1L2", "W1L1","L","Count of corrected results",
                    "Number of games played","Correct probability")
 
